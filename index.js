23bd00ae4a63f2d563cef15fd503cbc7aa49631a
David J Towster <djt4xe@virginia.edu>
2017-11-30 19:04:39 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  var ranks = document.querySelectorAll('.ranked-list li');  
+  for (var i = 0; i < ranks.length; i++) {
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  var current = document.querySelector('div#grand-node');
+  var next = current.children[0];
+  while (next) {
+    current = next;
+    next = current.children[0];
+  }
+  return current;
+}
